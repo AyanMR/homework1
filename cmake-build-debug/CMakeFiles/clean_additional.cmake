38c98adb479a57da56c7c2ba419729cee393dd1a
阎 阿 <3120476736@qq.com>
2025-05-04 03:57:57 +0800

    2

--- cmake-build-debug/CMakeFiles/clean_additional.cmake
+++ cmake-build-debug/CMakeFiles/clean_additional.cmake
@@ -5,9 +5,9 @@ if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
   "CMakeFiles\\homework1_autogen.dir\\AutogenUsed.txt"
   "CMakeFiles\\homework1_autogen.dir\\ParseCache.txt"
-  "CMakeFiles\\qt6_demo_autogen.dir\\AutogenUsed.txt"
-  "CMakeFiles\\qt6_demo_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\main.cpp_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\main.cpp_autogen.dir\\ParseCache.txt"
   "homework1_autogen"
-  "qt6_demo_autogen"
+  "main.cpp_autogen"
   )
 endif()
--- cmake-build-debug/homework1_autogen/include/ui_MainWindow.h
+++ cmake-build-debug/homework1_autogen/include/ui_MainWindow.h
@@ -12,6 +12,8 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -19,16 +21,33 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QVBoxLayout *verticalLayout;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
     QPushButton *pushButton;
 
     void setupUi(QWidget *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(870, 569);
+        MainWindow->resize(727, 537);
+        verticalLayout = new QVBoxLayout(MainWindow);
+        verticalLayout->setObjectName("verticalLayout");
+        radioButton = new QRadioButton(MainWindow);
+        radioButton->setObjectName("radioButton");
+
+        verticalLayout->addWidget(radioButton);
+
+        radioButton_2 = new QRadioButton(MainWindow);
+        radioButton_2->setObjectName("radioButton_2");
+
+        verticalLayout->addWidget(radioButton_2);
+
         pushButton = new QPushButton(MainWindow);
         pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(400, 480, 75, 24));
+
+        verticalLayout->addWidget(pushButton);
+
 
         retranslateUi(MainWindow);
 
@@ -38,7 +57,9 @@ public:
     void retranslateUi(QWidget *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "JXUFE\346\210\220\347\273\251\347\263\273\347\273\237", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "\346\237\245\350\257\242", nullptr));
+        radioButton->setText(QCoreApplication::translate("MainWindow", "\346\216\222\350\241\214\346\246\234", nullptr));
+        radioButton_2->setText(QCoreApplication::translate("MainWindow", "\344\277\256\346\224\271\346\210\220\347\273\251", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "\347\241\256\345\256\232", nullptr));
     } // retranslateUi
 
 };
